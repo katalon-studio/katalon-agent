1fb63c0e5e895031b2d3a7706134db3bde09f533
Thong Nguyen <thongmgnguyen@kms-technology.com>
2019-03-17 11:28:25 +0700

    configlog

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,8 @@
 var log4js = require('log4js');
 
+log4js.configure('./config/log-config.json');
 var logger = log4js.getLogger('katalon');
-logger.level = 'info';
+logger.level = 'all';
+
+ 
 module.exports = logger;
