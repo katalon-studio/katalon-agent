61c2cb4d5ad218077ca5c0f8f085e8fc4be2f2ad
Alex Smith <dev.alex.88@gmail.com>
2019-03-19 00:01:02 +0700

    update

--- test/katalon-studio.js
+++ test/katalon-studio.js
@@ -1,9 +1,17 @@
 const ks = require('../src/katalon-studio');
 
+// ks.execute(
+//   '5.10.1',
+//   null,
+//   'C:\\data\\docker-images-samples',
+//   '-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"',
+//   null,
+//   null);
+
 ks.execute(
-  '6.1.0',
-  'C:\\data\\katalon-studio-6.1.0',
-  'C:\\data\\docker-images-samples',
+  '5.10.1',
+  null,
+  '/home/hai/ci-samples',
   '-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"',
   null,
-  null);
+  '-a -n 0 -s "-screen 0 1024x768x24"');
