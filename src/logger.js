097a7959d0b4e3134f7354b342c281cb7d4ee0e7
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-03-26 14:35:28 +0700

    Remove log config

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,36 @@
 var log4js = require('log4js');
 
-log4js.configure('./config/log-config.json');
+logConfigs = {
+    appenders: {
+        access: {
+            type: "dateFile",
+            filename: "log/access.log",
+            pattern: "-yyyy-MM-dd",
+            category: "http",
+        },
+        out: { type: "stdout", },
+        app: {
+            type: "file",
+            filename: "log/app.log",
+            maxLogSize: 10485760,
+            numBackups: 3,
+        },
+        errorFile: {
+            type: "file",
+            filename: "log/errors.log",
+        },
+        errors: {
+            type: "logLevelFilter",
+            level: "ERROR",
+            appender: "errorFile",
+        },
+    },
+    categories: {
+        default: { appenders: ["app", "errors", "out"], level: "DEBUG", },
+    },
+}
+
+log4js.configure(logConfigs);
 var logger = log4js.getLogger('katalon');
 logger.level = 'all';
 
