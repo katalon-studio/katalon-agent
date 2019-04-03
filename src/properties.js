b4baadd68afda365186f30dfbbc33232c16b0afa
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-04-03 23:09:31 +0700

    Fix several eslint errors

--- src/properties.js
+++ src/properties.js
@@ -1,16 +1,16 @@
 const fse = require('fs-extra');
-const logger = require('./logger');
 const properties = require('properties');
+const logger = require('./logger');
 
 function writeProperties(propertiesFile, prop) {
-    fse.ensureFileSync(propertiesFile);
-    properties.stringify(prop, { path: propertiesFile, }, (err, str) => {
-        if (err) {
-            return logger.error(err);
-        }
+  fse.ensureFileSync(propertiesFile);
+  properties.stringify(prop, { path: propertiesFile }, (err, str) => {
+    if (err) {
+      return logger.error(err);
+    }
 
-        logger.trace("Write properties:\n", str);
-    });
+    return logger.trace('Write properties:\n', str);
+  });
 }
 
 module.exports.writeProperties = writeProperties;
