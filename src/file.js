f4ead763ec6eb2e2dbb50b156b0dd7c9067df4cc
Black <tuananhtran@kms-technology.com>
2019-04-25 16:29:21 +0700

    fix filter

--- src/file.js
+++ src/file.js
@@ -7,7 +7,7 @@ const defaultLogger = require('./logger');
 module.exports = {
   extract(filePath, targetDir, haveFilter, logger = defaultLogger) {
     logger.info(`Decompressing the ${filePath} into ${targetDir}.`);
-    return decompress(filePath, targetDir, haveFilter, {
+    return decompress(filePath, targetDir, {
       filter: (decompressFile) => {
         if (haveFilter) {
           const decompressPath = decompressFile.path;
