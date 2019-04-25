532c182bdeabcbd977f72c0db2d2739dda2883ab
Black <tuananhtran@kms-technology.com>
2019-04-25 10:29:58 +0700

    'update'

--- src/file.js
+++ src/file.js
@@ -5,12 +5,16 @@ const http = require('./http');
 const defaultLogger = require('./logger');
 
 module.exports = {
-  extract(filePath, targetDir, logger = defaultLogger) {
+  extract(filePath, targetDir, haveFilter, logger = defaultLogger) {
     logger.info(`Decompressing the ${filePath} into ${targetDir}.`);
-    return decompress(filePath, targetDir, {
+    return decompress(filePath, targetDir, haveFilter, {
       filter: (decompressFile) => {
-        const decompressPath = decompressFile.path;
-        return !decompressPath.includes('.git/') && !decompressPath.includes('__MACOSX');
+        if (haveFilter) {
+          const decompressPath = decompressFile.path;
+          return !decompressPath.includes('.git/') && !decompressPath.includes('__MACOSX');
+        } else {
+          return decompressFile;
+        }
       },
     });
   },
@@ -20,18 +24,6 @@ module.exports = {
     const file = tmp.fileSync();
     const filePath = file.name;
     return http.stream(url, filePath)
-      .then(() => {
-        logger.info(`Decompressing the file into ${targetDir}.`);
-        return decompress(filePath, targetDir, {
-          filter: (decompressFile) => {
-            if (haveFilter) {
-              const decompressPath = decompressFile.path;
-              return !decompressPath.includes('.git/') && !decompressPath.includes('__MACOSX');
-            } else {
-              return decompressFile;
-            }
-          },
-        });
-      });
+      .then(() => this.extract(filePath, targetDir, haveFilter, logger));
   },
 };
