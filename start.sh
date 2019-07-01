3e05d5700d41fef002078e7c48ee8e6fabfe6c4f
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-07-01 15:51:22 +0700

    Update start script

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ case `uname -m` in
   *) BIN="cli-$OS-x86";;
 esac
 
+if [ $BIN == "cli-macos-x86" ]; then
+  echo "32-bit MacOS is not supported."
+  exit 0
+fi
+
 CURRENT_DIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd -P)"
 
 ${CURRENT_DIR}/${BIN} start-agent
