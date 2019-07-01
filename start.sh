6490e3f979909afa7eb06fddc2f4e6c6b72885fb
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-07-01 15:39:46 +0700

    Update start script

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-case `arch` in
-  *64*) BIN="cli-linux-x64";;
-  *) BIN="cli-linux-x86";;
+case `uname` in
+  *Darwin*) OS="macos";;
+  *) OS="linux";;
+esac
+
+case `uname -m` in
+  *64*) BIN="cli-$OS-x64";;
+  *) BIN="cli-$OS-x86";;
 esac
 
 CURRENT_DIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd -P)"
