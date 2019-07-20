e4d7866107c6aee91e49684cd1dc02dc63548f12
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-07-21 00:48:51 +0700

    Add release-it config

---
+++ .release-it.js
@@ -0,0 +1,23 @@
+const version = process.env.npm_package_version;
+const user = 'sonhuynhpham';
+const token = '${GITHUB_TOKEN}';
+
+module.exports = {
+    git: {
+      pushRepo: `https://${user}:${token}@github.com/katalon-studio/katalon-agent.git`,
+      changelog: 'git log -1 --pretty=format:"%s"',
+      tagName: `v${version}`,
+      requireCleanWorkingDir: false,
+      tag: false,
+      commit: false,
+      push: false,
+    },
+    github: {
+        release: true,
+        releaseName: `Release ${version} 🚧`,
+        draft: true,
+        assets: ['bin/agentconfig', 'bin/*x64*', 'service.*', 'start.*', 'nssm.exe'],
+    },
+    npm: false,
+    increment: false,
+}
