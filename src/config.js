344833987849a146172f418bc4123e16eb0876e7
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-10-21 10:35:58 +0700

    Refactor config

--- src/config.js
+++ src/config.js
@@ -1,43 +1,31 @@
 const _ = require('lodash');
-const fs = require('fs');
 const fse = require('fs-extra');
 const ini = require('ini');
 const path = require('path');
 
-const logger = require('./logger');
-
 const configFile = path.resolve(process.cwd(), 'config.ini');
 let global = {};
-
-function isConfigFileEmpty() {
-  let empty = true;
-  if (fs.existsSync(configFile)) {
-    const configs = ini.parse(fs.readFileSync(configFile, 'utf-8'));
-    empty = _.isEmpty(configs);
-  }
-  if (empty) {
-    logger.debug('config file is empty');
-  }
-  return empty;
-}
 
 // NOTE: ONLY EXPORT FUNCTIONS, DO NOT EXPORT FIELDS
 module.exports = {
   update(commandLineConfigs, filepath = configFile) {
     /* Update the module with configs read from both config file and command line */
     // Filter undefined fields
-    commandLineConfigs = _.pickBy(commandLineConfigs, value => value !== undefined);
+    commandLineConfigs = _.pickBy(commandLineConfigs, (value) => value !== undefined);
     // Read configs from file
     const fileConfigs = this.read(filepath);
     // Merge both configs
-    const configs = _.extend({}, fileConfigs, commandLineConfigs,
-      { pathPatterns: _.get(fileConfigs, 'paths.path', []) });
+    const configs = {
+      ...fileConfigs,
+      ...commandLineConfigs,
+      pathPatterns: _.get(fileConfigs, 'paths.path', []),
+    };
 
     // Add configs to global and export configs
     global = _.extend(global, configs);
-    for (let p in global) {
+    Object.keys(global).forEach((p) => {
       module.exports[p] = global[p];
-    }
+    });
   },
 
   read(filepath) {
@@ -48,7 +36,7 @@ module.exports = {
 
   write(filepath, configs) {
     // Filter undefined and function fields
-    configs = _.pickBy(configs, value => !_.isUndefined(value) && !_.isFunction(value));
+    configs = _.pickBy(configs, (value) => !_.isUndefined(value) && !_.isFunction(value));
     const outputINI = ini.stringify(configs);
     fse.outputFileSync(filepath, outputINI);
   },
