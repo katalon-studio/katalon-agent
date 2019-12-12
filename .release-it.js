13f5229559f4bfb6f3ef1223144749c7b5cfcfd8
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-12-12 11:47:43 +0700

    Add github actions script

--- .release-it.js
+++ .release-it.js
@@ -1,23 +1,36 @@
-const version = process.env.npm_package_version;
-const user = 'sonhuynhpham';
-const token = '${GITHUB_TOKEN}';
+const {
+  npm_package_version: version,
+  JOB_BASE_NAME: jobName = "",
+  BUILD_DISPLAY_NAME: buildName = ""
+} = process.env;
+
+const buildInfo = jobName ? ` Build ${jobName}${buildName}` : "";
 
 module.exports = {
-    git: {
-      pushRepo: `https://${user}:${token}@github.com/katalon-studio/katalon-agent.git`,
-      changelog: 'git log -1 --pretty=format:"%s%n%n>**Commit:** %h%d%n>**Author:** %an%n>**Date:** %ai%n"',
-      tagName: `v${version}`,
-      requireCleanWorkingDir: false,
-      tag: false,
-      commit: false,
-      push: false,
-    },
-    github: {
-        release: true,
-        releaseName: `Release ${version} 🚧`,
-        draft: true,
-        assets: ['bin/agentconfig', 'bin/*x64*', 'service.*', 'start.*', 'nssm.exe'],
-    },
-    npm: false,
-    increment: false,
-}
+  git: {
+    pushRepo: "https://github.com/katalon-studio/katalon-agent.git",
+    changelog:
+      'git log -1 --pretty=format:"%s%n%n>**Commit:** %h%d%n>**Author:** %an%n>**Date:** %ai%n"',
+    tagName: `v${version}`,
+    requireCleanWorkingDir: false,
+    tag: false,
+    commit: false,
+    push: false,
+    requireUpstream: false
+  },
+  github: {
+    release: true,
+    releaseName: `Release ${version} 🚧${buildInfo}`,
+    draft: true,
+    assets: [
+      "bin/agentconfig",
+      "bin/*x64*",
+      "bin/*x86*",
+      "service.*",
+      "start.*",
+      "nssm.exe"
+    ]
+  },
+  npm: false,
+  increment: false
+};
