67ce898eb1b9b9e0999ed2f50a6c83cfe0f92d9c
Qui Le <qui.le@katalon.com>
2021-01-04 17:39:47 +0700

    update

--- docker/scripts/agent.sh
+++ docker/scripts/agent.sh
@@ -9,7 +9,18 @@ cat $KATALON_VERSION_FILE
 
 cd $KATALON_AGENT_DIR
 
-./cli-linux-x64 config --server-url "$SERVER_URL" --username "$KATALON_USERNAME" --apikey "$KATALON_API_KEY" --teamid "$TEAM_ID" --agent-name "$AGENT_NAME"
+./cli-linux-x64 config \
+    --server-url "$SERVER_URL" \
+    --username "$KATALON_USERNAME" \
+    --apikey "$KATALON_API_KEY" \
+    --teamid "$TEAM_ID" \
+    --agent-name "$AGENT_NAME" \
+    --proxy "$PROXY" \
+    --log-level "$LOG_LEVEL" \
+    --xvfb-run "$XVFB_RUN" \
+    --x11-display "$X11_DISPLAY" \
+    --keep-files "$KEEP_FILES" \
+    --no-keep-files "$NO_KEEP_FILES"
 args=("./cli-linux-x64" "$@")
 
 xvfb-run -s "-screen 0 $DISPLAY_CONFIGURATION" "${args[@]}"
