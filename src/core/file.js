832c9ed0835569f72585f3770cc2b50917bd86e1
sonhuynhpham <sonhuynhpham@kms-technology.com>
2021-01-26 17:14:34 +0700

    Add a separate download and extract from TO and refactor to support TO download url

--- src/core/file.js
+++ src/core/file.js
@@ -6,6 +6,22 @@ const tmp = require('tmp');
 const api = require('./api');
 const defaultLogger = require('../config/logger');
 
+function download(downloadMethod, url, logger = defaultLogger) {
+  logger.info(`Downloading from ${url}. It may take a few minutes.`);
+  const file = tmp.fileSync();
+  const filePath = file.name;
+  logger.debug(`Download into temporary directory: ${filePath}`);
+  const verifyDownloadedFile = (res) => {
+    const { body } = res;
+    if (body && body.filePath) {
+      return Promise.resolve(body.filePath);
+    }
+    return Promise.reject(new Error(`Unable to download from ${url} to ${filePath}`));
+  };
+  return downloadMethod(url, filePath)
+    .then(verifyDownloadedFile);
+}
+
 module.exports = {
   extract(filePath, targetDir, haveFilter, logger = defaultLogger) {
     logger.info(`Decompressing the ${filePath} into ${targetDir}.`);
@@ -21,22 +37,19 @@ module.exports = {
   },
 
   downloadAndExtract(url, targetDir, haveFilter = false, logger = defaultLogger) {
-    logger.info(`Downloading from ${url}. It may take a few minutes.`);
-    const file = tmp.fileSync();
-    const filePath = file.name;
-    logger.debug(`Download into temporary directory: ${filePath}`);
-    return api
-      .download(url, filePath)
-      .then(() => this.extract(filePath, targetDir, haveFilter, logger));
+    return download(api.download, url, logger).then((filePath) =>
+      this.extract(filePath, targetDir, haveFilter, logger),
+    );
+  },
+
+  downloadAndExtractFromTestOps(url, targetDir, haveFilter = false, logger = defaultLogger) {
+    return download(api.downloadFromTestOps, url, logger).then((filePath) =>
+      this.extract(filePath, targetDir, haveFilter, logger),
+    );
   },
 
   clone(gitRepository, targetDir, cloneOpts = {}, logger = defaultLogger) {
-    const {
-      repository,
-      branch,
-      username,
-      password,
-    } = gitRepository || {};
+    const { repository, branch, username, password } = gitRepository || {};
 
     const repoURL = new URL(repository);
     repoURL.username = username;
@@ -45,7 +58,9 @@ module.exports = {
 
     const dirName = url.split('/').pop();
     const gitTargetDir = path.join(targetDir, dirName);
-    logger.info(`Cloning from ${repository} (${branch}) into ${gitTargetDir}. It may take a few minutes.`);
+    logger.info(
+      `Cloning from ${repository} (${branch}) into ${gitTargetDir}. It may take a few minutes.`,
+    );
 
     const overrideOpts = Object.entries(cloneOpts).reduce((opts, [k, v]) => {
       opts.push(k);
