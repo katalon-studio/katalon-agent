993869cba410eeacf9f9e58821cab2a5781a2982
Trai Le <trai.le@katalon.com>
2021-12-30 09:38:36 +0700

    Update message when moved extra files

--- src/core/file.js
+++ src/core/file.js
@@ -41,7 +41,7 @@ module.exports = {
     logger.info(`Moving the ${filePath} into ${targetDir}.`);
     fs.move(filePath, targetDir, (err) => {
       if (err) return logger.error(`Can not move Test Suite ${err}`);
-      logger.info('Updated script repository with Test Suite from TestOps');
+      logger.info('Updated script repository with extra files from TestOps');
       return true;
     });
   },
