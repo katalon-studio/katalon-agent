580d38f5d37bd30bfbf16dcfb2920969f62232e5
quidl <43738052+quidl@users.noreply.github.com>
2023-06-16 14:59:26 +0700

    Use katalon studio base image

---
+++ docker/scripts/upgrade_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -xe
+
+echo "Upgrade Google Chrome"
+chrome_package='google-chrome-stable_current_amd64.deb'
+wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i $chrome_package || apt -y -f install
+rm $chrome_package
+echo "$(google-chrome --version)" >> $KATALON_VERSION_FILE || true
