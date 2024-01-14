8a44a54ae8f538ded3111dab7497bbb2adf0707f
lydoan1199 <ly.doan@katalon.com>
2024-01-14 14:14:12 +0700

    debug

--- docker/scripts/upgrade_environment.sh
+++ docker/scripts/upgrade_environment.sh
@@ -3,8 +3,8 @@
 set -xe
 
 echo "Upgrade Google Chrome"
-chrome_package='google-chrome-stable_current_amd64.deb'
-wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+chrome_package='google-chrome-stable_current_arm64.deb'
+wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_arm64.deb
 dpkg -i $chrome_package || apt -y -f install
 rm $chrome_package
 ./wrap_chrome_binary.sh
