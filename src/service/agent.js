f516041b96fe459cdf3a6f9d4a0569b64a8e9545
Qui Le <43738052+quidl@users.noreply.github.com>
2024-04-18 20:00:34 +0700

    Update (#200)
    Upgrade lib

--- src/service/agent.js
+++ src/service/agent.js
@@ -229,6 +229,14 @@ async function executeJob(jobInfo, keepFiles) {
     // Remove temporary directory when `keepFiles` is false
     if (!keepFiles) {
       tmpDir.removeCallback();
+      // try {
+      //   console.log('QQQQQQ1 tmpDirPath', tmpDirPath);
+      //   console.log('QQQQQQ1 fs', fs);
+      //   fs.rmSync(tmpDirPath, { recursive: true, force: true });
+      //   console.log('QQQQQQ2 tmpDirPath', tmpDirPath);
+      // } catch (err) {
+      //   logger.error('Error when removing tmp directory:', err);
+      // }
     }
   }
 }
