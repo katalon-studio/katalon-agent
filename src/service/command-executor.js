14768ca5df93f668d42314a93b37fa4c10782264
quidl <43738052+quidl@users.noreply.github.com>
2024-04-19 14:28:00 +0700

    Fix html

--- src/service/command-executor.js
+++ src/service/command-executor.js
@@ -61,6 +61,9 @@ class BaseKatalonCommandExecutor {
     const projectPathPattern = path.resolve(execDirPath, PROJECT_FILE_PATTERN);
     const ksProjectPaths = glob.sync(projectPathPattern, { nodir: true });
 
+    logger.info(`QQQQQQQQ execDirPath: ${execDirPath}.`);
+    logger.info(`QQQQQQQQ projectPathPattern: ${projectPathPattern}.`);
+    logger.info(`QQQQQQQQ ksProjectPaths: ${ksProjectPaths}.`);
     if (ksProjectPaths.length <= 0) {
       logger.error('Unable to find a Katalon project.');
       return Promise.resolve(1);
