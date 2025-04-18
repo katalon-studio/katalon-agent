335eaadfc26dfacd7cd6ba4b9f9df880a0810426
Phat Cu <phat.tcu@katalon.com>
2025-04-18 16:15:25 +0700

    Add script auto upgrade firefox version

--- docker/scripts/upgrade_environment.sh
+++ docker/scripts/upgrade_environment.sh
@@ -2,6 +2,21 @@
 
 set -xe
 
+echo "Upgrade Mozilla Firefox"
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | tee /etc/apt/preferences.d/mozilla
+apt-get update
+apt-get install firefox -y
+# Install 'pulseaudio' package to support WebRTC audio streams
+apt -y install pulseaudio
+echo "$(firefox -version)" >> $KATALON_VERSION_FILE
+
 echo "Upgrade Google Chrome"
 chrome_package='google-chrome-stable_current_amd64.deb'
 wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
