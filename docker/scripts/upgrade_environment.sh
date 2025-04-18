8ce5a3a18e1ac7305a28d9efc4becbec548099d3
Phat Cu <phat.tcu@katalon.com>
2025-04-18 15:38:49 +0700

    Add script auto upgrade microsoft edge version

--- docker/scripts/upgrade_environment.sh
+++ docker/scripts/upgrade_environment.sh
@@ -9,3 +9,11 @@ dpkg -i $chrome_package || apt -y -f install
 rm $chrome_package
 ./wrap_chrome_binary.sh
 echo "$(google-chrome --version)" >> $KATALON_VERSION_FILE || true
+
+echo "Upgrade Edge Chromium"
+microsoft_edge_package='microsoft-edge-stable_current_amd64.deb'
+wget -O $microsoft_edge_package https://go.microsoft.com/fwlink?linkid=2149051
+dpkg -i $microsoft_edge_package || apt -y -f install
+rm $microsoft_edge_package
+./wrap_edge_chromium_binary.sh
+echo "$(microsoft-edge --version)" >> $KATALON_VERSION_FILE || true
